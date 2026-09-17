c0f15da2e05a39f7bcbe1c5e44c820cc1427d8b7
Andre <dremartinss07@gmail.com>
2026-09-17 22:50:13 +0000

    done with swap and printvalues

--- bubbleSort.c
+++ bubbleSort.c
@@ -10,18 +10,34 @@ int main(){
   printf("Before: \n");
   printValues(values);
 
- /* test swap
+
+   // test swap
   int x = 3;
   int y = 5;
   printf("x: %d, y: %d \n", x, y);
   swap(&x, &y);
   printf("x: %d, y: %d \n", x, y);
 
-  sort(values);
-  printf("After: \n");
-  printValues(values);
-*/
-  return(0);
+ // sort(values);
+ // printf("After: \n");
+ // printValues(values);
+
+
+return(0);
 } // end main
 	
-int printValues(int*)
+void printValues(int* values){
+	printf("[");
+	for (int i = 0; i < MAX; i++){
+		printf("%d ", values[i]);
+	}
+	printf("]");
+}
+
+
+void swap(int* x, int* y){
+	int temp = *x;
+	*x = *y;
+	*x = temp;
+	
+} 
